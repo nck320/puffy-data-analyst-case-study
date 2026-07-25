1c1e1f04c556678d1cedd907fb4e2d93b2b09495
nck320 <nihalrajeevsainudeen@gmail.com>
2026-07-25 19:11:59 +0400

    final_deploy

--- sql_app.py
+++ sql_app.py
@@ -54,8 +54,8 @@ st.markdown(
         background-color: #0E1117;
         color: #94A3B8;
         text-align: center;
-        padding: 16px;
-        font-size: 2.0rem;
+        padding: 14px;
+        font-size: 1.5rem;
         border-top: 1px solid #1E293B;
         z-index: 999;
     }
